5132739eb39a6b6f6eb10f23954dd08055d9cd4a
Zeljko Predjeskovic <zepr2022000@gmail.com>
2021-06-26 11:02:32 +0200

    added women annual income and both

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Javafx gui for visualising the annual income per person in austria
 
+### Source for the statistics: https://www.statistik.at/web_de/statistiken/menschen_und_gesellschaft/soziales/personen-einkommen/jaehrliche_personen_einkommen/index.html
+
+
 
--- src/viewModel/Controller.java
+++ src/viewModel/Controller.java
@@ -1,10 +1,12 @@
 package viewModel;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.GridPane;
 import model.service.AnnualIncomeService;
 
@@ -27,16 +29,45 @@ public class Controller implements Initializable {
     @FXML
     NumberAxis yAxis;
 
+    @FXML
+    ComboBox comboBox;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        comboBox.getItems().addAll("Men's Yearly Income",
+                "Women's Yearly Income",
+                "Both Yearly Income");
+
         xAxis.setAutoRanging(false);
         xAxis.setLowerBound(1995);
         xAxis.setUpperBound(2021);
         xAxis.setTickUnit(1);
         yAxis.setAutoRanging(false);
-        yAxis.setLowerBound(25000);
+        yAxis.setLowerBound(15000);
         yAxis.setUpperBound(45000);
         yAxis.setTickUnit(5000);
+    }
+
+    public void comboBox(){
+        switch (comboBox.getValue().toString()){
+            case "Men's Yearly Income" :
+                lineChart.getData().clear();
+                menIncome();
+                break;
+            case "Women's Yearly Income" :
+                lineChart.getData().clear();
+                womenIncome();
+                break;
+            case "Both Yearly Income" :
+                lineChart.getData().clear();
+                bothIncome();
+                break;
+            default:
+                System.out.println("No item clicked");
+        }
+    }
+
+    public void menIncome(){
         lineChart.setTitle("Annual Income for Men in Austria");
         //defining a series
         XYChart.Series series = new XYChart.Series();
@@ -48,5 +79,25 @@ public class Controller implements Initializable {
 
         lineChart.getData().add(series);
 
+    }
+
+    public void womenIncome(){
+        lineChart.setTitle("Annual Income for Women in Austria");
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Women's Annual Income/Year");
+        //populating the series with data
+        for (int i = 0; i<women.getAllValues().size(); i++){
+            series.getData().add(new XYChart.Data(women.getAllKeys().get(i), women.getAllValues().get(i)));
+        }
+
+        lineChart.getData().add(series);
+
+    }
+
+    public void bothIncome(){
+        menIncome();
+        womenIncome();
+        lineChart.setTitle("Annual Income for Women and Men in Austria");
     }
 }
